09f3cdfe511f03b1b008117ed0aa321d884c5f23
textheadmin <textheadmin@users.noreply.github.com>
2015-06-09 15:45:21 -0500

    Added addition info in interface

--- addon.py
+++ addon.py
@@ -1,20 +1,19 @@
-import sys
 import xbmcgui
 import xbmcplugin
 import xbmc
 import xbmcaddon
 
-addon_handle = int(sys.argv[1])
+
 
 xbmcplugin.setContent(addon_handle, 'audio')
 
 addon = xbmcaddon.Addon('plugin.audio.air1-radio')
 title = addon.getAddonInfo('name')
-#icon = addon.getAddonInfo('icon')
+icon = 'DefaultAddonMusic.png'
 url = "rtmp://emf.mpl.miisolutions.net/sa001-high-fms-live01/sa001_mii_edge_high.stream"
 
-li = xbmcgui.ListItem(label=title, path=url)
-li.setInfo(type='Audio', infoLabels={ "Title": title })
+li = xbmcgui.ListItem(label=title, iconImage=icon, thumbnailImage=icon, path=url)
+li.setInfo(type='music', infoLabels={ "Title": title })
 li.setProperty('IsPlayable', 'true')
 
 xbmc.Player().play(item=url, listitem=li)
